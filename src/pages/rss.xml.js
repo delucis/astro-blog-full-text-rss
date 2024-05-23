d3b11f1461f3102a2b3b4919f971abd01626f0c6
Chris Swithinbank <swithinbank@gmail.com>
2024-05-23 13:20:15 +0000

    Make links and image sources absolute URLs

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,15 @@
+// @ts-check
+
 import rss from '@astrojs/rss';
 import { experimental_AstroContainer as AstroContainer } from 'astro/container';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 import Renderer from './_rss/ContentRenderer.astro';
 import sanitize from 'sanitize-html';
+import { ELEMENT_NODE, TEXT_NODE, transform, walk } from "ultrahtml";
+
+let baseUrl = import.meta.env.SITE;
+if (baseUrl.at(-1) === '/') baseUrl = baseUrl.slice(0, -1);
 
 const container = await AstroContainer.create({
   renderers: [
@@ -23,8 +29,19 @@ export async function GET(context) {
         let content = await container.renderToString(Renderer, {
           params: { slug },
         });
+        content = await transform(content, [async (node) => {
+          await walk(node, (node) => {
+            // Make sure links are absolute, some readers are not smart enough to figure it out
+            if (node.name === "a" && node.attributes.href?.startsWith("/")) {
+              node.attributes.href = baseUrl + node.attributes.href;
+            }
+            if (node.name === 'img' && node.attributes.src?.startsWith('/')) {
+              node.attributes.src = baseUrl + node.attributes.src;
+            }
+          })
+          return node;
+        }]);
         // Sanitize HTML but allow for <img> tags.
-        // TODO: make img `src` attributes absolute.
         content = sanitize(content, {
           allowedTags: sanitize.defaults.allowedTags.concat(['img']),
         });
