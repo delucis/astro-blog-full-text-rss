8786a1c5d2e009e26ac804ff06b21f9c92fe1c14
Chris Swithinbank <swithinbank@gmail.com>
2024-05-23 14:02:30 +0000

    Use ultrahtml for sanitization

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,24 +1,24 @@
 // @ts-check
 
-import rss from '@astrojs/rss';
-import { experimental_AstroContainer as AstroContainer } from 'astro/container';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
-import Renderer from './_rss/ContentRenderer.astro';
-import sanitize from 'sanitize-html';
-import { ELEMENT_NODE, TEXT_NODE, transform, walk } from "ultrahtml";
+import rss from "@astrojs/rss";
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { getCollection } from "astro:content";
+import { transform, walk } from "ultrahtml";
+import sanitize from "ultrahtml/transformers/sanitize";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+import Renderer from "./_rss/ContentRenderer.astro";
 
 let baseUrl = import.meta.env.SITE;
-if (baseUrl.at(-1) === '/') baseUrl = baseUrl.slice(0, -1);
+if (baseUrl.at(-1) === "/") baseUrl = baseUrl.slice(0, -1);
 
 const container = await AstroContainer.create({
   renderers: [
-    { name: '@astrojs/mdx', serverEntrypoint: 'astro/jsx/server.js' },
+    { name: "@astrojs/mdx", serverEntrypoint: "astro/jsx/server.js" },
   ],
 });
 
 export async function GET(context) {
-  const posts = await getCollection('blog');
+  const posts = await getCollection("blog");
   return rss({
     // stylesheet: '/styles.xsl',
     title: SITE_TITLE,
@@ -29,22 +29,23 @@ export async function GET(context) {
         let content = await container.renderToString(Renderer, {
           params: { slug },
         });
-        content = await transform(content, [async (node) => {
-          await walk(node, (node) => {
-            // Make sure links are absolute, some readers are not smart enough to figure it out
-            if (node.name === "a" && node.attributes.href?.startsWith("/")) {
-              node.attributes.href = baseUrl + node.attributes.href;
-            }
-            if (node.name === 'img' && node.attributes.src?.startsWith('/')) {
-              node.attributes.src = baseUrl + node.attributes.src;
-            }
-          })
-          return node;
-        }]);
-        // Sanitize HTML but allow for <img> tags.
-        content = sanitize(content, {
-          allowedTags: sanitize.defaults.allowedTags.concat(['img']),
-        });
+        // Thanks @Princesseuh — https://github.com/Princesseuh/erika.florist/blob/1827288c14681490fa301400bfd815acb53463e9/src/middleware.ts
+        content = await transform(content, [
+          async (node) => {
+            await walk(node, (node) => {
+              // Make sure links are absolute, some readers are not smart enough to figure it out
+              if (node.name === "a" && node.attributes.href?.startsWith("/")) {
+                node.attributes.href = baseUrl + node.attributes.href;
+              }
+              // Same thing but for image src attributes
+              if (node.name === "img" && node.attributes.src?.startsWith("/")) {
+                node.attributes.src = baseUrl + node.attributes.src;
+              }
+            });
+            return node;
+          },
+          sanitize({ dropElements: ["script", "style"] }),
+        ]);
         return { ...data, link: `/blog/${slug}/`, content };
       })
     ),
