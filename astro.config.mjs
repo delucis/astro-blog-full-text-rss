d76864779fe07e9cdc053319a97204d1b93c99f1
Chris Swithinbank <swithinbank@gmail.com>
2024-05-23 13:21:09 +0000

    Add correct `site` URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://astro-blog-full-text-rss.vercel.app',
 	integrations: [mdx(), sitemap()],
 });
