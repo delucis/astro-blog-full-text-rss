ae7cf72b0bb04bac9586da2440fb0a824ce3b7ef
trueberryless <trueberryless+github@gmail.com>
2025-02-17 15:32:26 +0100

    update to Astro v5

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -38,7 +38,7 @@ export async function GET(context: APIContext) {
     // - Makes link `href` and image `src` attributes absolute instead of relative
     // - Strips any `<script>` and `<style>` tags
     // Thanks @Princesseuh — https://github.com/Princesseuh/erika.florist/blob/1827288c14681490fa301400bfd815acb53463e9/src/middleware.ts
-    const content = await transform(rawContent.replace(/^<!DOCTYPE html>/, ''), [
+    const content = await transform(rawContent.replace(/^<!DOCTYPE html>/, ""), [
       async (node) => {
         await walk(node, (node) => {
           if (node.name === "a" && node.attributes.href?.startsWith("/")) {
@@ -52,7 +52,7 @@ export async function GET(context: APIContext) {
       },
       sanitize({ dropElements: ["script", "style"] }),
     ]);
-    feedItems.push({ ...post.data, link: `/blog/${post.slug}/`, content });
+    feedItems.push({ ...post.data, link: `/blog/${post.id}/`, content });
   }
 
   // Return our RSS feed XML response.
