f920aee1c1909e9456c93d97db0c3bfc8288b5a2
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2025-11-28 11:04:37 +0100

    Upgrade to Astro 5 (#14)
    Co-authored-by: Chris Swithinbank <swithinbank@gmail.com>

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,7 +3,7 @@ import rss, { type RSSFeedItem } from "@astrojs/rss";
 import type { APIContext } from "astro";
 import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import { loadRenderers } from "astro:container";
-import { getCollection } from "astro:content";
+import { getCollection, render } from "astro:content";
 import { transform, walk } from "ultrahtml";
 import sanitize from "ultrahtml/transformers/sanitize";
 import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
@@ -30,7 +30,7 @@ export async function GET(context: APIContext) {
   const feedItems: RSSFeedItem[] = [];
   for (const post of posts) {
     // Get the `<Content/>` component for the current post.
-    const { Content } = await post.render();
+    const { Content } = await render(post);
     // Use the Astro container to render the content to a string.
     const rawContent = await container.renderToString(Content);
     // Process and sanitize the raw content:
@@ -38,7 +38,7 @@ export async function GET(context: APIContext) {
     // - Makes link `href` and image `src` attributes absolute instead of relative
     // - Strips any `<script>` and `<style>` tags
     // Thanks @Princesseuh — https://github.com/Princesseuh/erika.florist/blob/1827288c14681490fa301400bfd815acb53463e9/src/middleware.ts
-    const content = await transform(rawContent.replace(/^<!DOCTYPE html>/, ''), [
+    const content = await transform(rawContent.replace(/^<!DOCTYPE html>/, ""), [
       async (node) => {
         await walk(node, (node) => {
           if (node.name === "a" && node.attributes.href?.startsWith("/")) {
@@ -52,7 +52,7 @@ export async function GET(context: APIContext) {
       },
       sanitize({ dropElements: ["script", "style"] }),
     ]);
-    feedItems.push({ ...post.data, link: `/blog/${post.slug}/`, content });
+    feedItems.push({ ...post.data, link: `/blog/${post.id}/`, content });
   }
 
   // Return our RSS feed XML response.
